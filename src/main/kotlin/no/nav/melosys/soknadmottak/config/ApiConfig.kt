206cce7b15efb44ad664ea61a006cdd2b146d47d
Francois Allix <francois.allix@nav.no>
2020-07-29 13:38:05 +0200

    Flytter config under config

--- src/main/kotlin/no/nav/melosys/soknadmottak/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/melosys/soknadmottak/config/ApiConfig.kt
@@ -1,4 +1,4 @@
-package no.nav.melosys.soknadmottak.api
+package no.nav.melosys.soknadmottak.config
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
--- src/main/kotlin/no/nav/melosys/soknadmottak/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/melosys/soknadmottak/config/DatabaseConfig.kt
@@ -1,4 +1,4 @@
-package no.nav.melosys.soknadmottak.database
+package no.nav.melosys.soknadmottak.config
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
