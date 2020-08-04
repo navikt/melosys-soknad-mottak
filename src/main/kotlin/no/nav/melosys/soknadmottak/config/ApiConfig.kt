07b5301dc2fda631ca024b073578cbf3ff763fe7
Francois Allix <francois.allix@nav.no>
2020-08-04 16:29:33 +0200

    Migrering fra Springfox 2 til Springfox 3

--- src/main/kotlin/no/nav/melosys/soknadmottak/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/melosys/soknadmottak/config/ApiConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import springfox.documentation.builders.PathSelectors
@@ -40,7 +41,7 @@ class ApiConfig : WebMvcConfigurer {
 }
 
 @Configuration
-@EnableSwagger2
+@EnableWebMvc
 @ConditionalOnProperty(name = ["NAIS_CLUSTER_NAME"], havingValue = "dev-fss", matchIfMissing = true)
 class SwaggerConfig {
     @Bean
