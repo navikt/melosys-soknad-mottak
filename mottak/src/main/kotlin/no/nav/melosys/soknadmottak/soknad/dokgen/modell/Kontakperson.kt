77db2f6b982f5abda96c79090daa3d427d7a7b28
Francois Allix <francois.allix@nav.no>
2020-11-27 10:18:13 +0100

    Telefonnummer er valgfritt

--- mottak/src/main/kotlin/no/nav/melosys/soknadmottak/soknad/dokgen/modell/Kontakperson.kt
+++ mottak/src/main/kotlin/no/nav/melosys/soknadmottak/soknad/dokgen/modell/Kontakperson.kt
@@ -2,7 +2,7 @@ package no.nav.melosys.soknadmottak.soknad.dokgen.modell
 
 data class Kontakperson(
     val navn: String,
-    val telefon: String,
+    val telefon: String?,
     val ansattHos: String,
     val harFullmakt: Boolean,
     val fullmektigVirksomhetsnummer: String,
