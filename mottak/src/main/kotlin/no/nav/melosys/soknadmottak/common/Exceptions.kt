345c26925eab05e463043fe1ac63aeb08dd8c618
Eivind Nilsbakken <eivind.nilsbakken@nav.no>
2020-12-09 22:38:15 +0100

    Fjern unødvendig logging + logg feil ved kvittering

--- mottak/src/main/kotlin/no/nav/melosys/soknadmottak/common/Exceptions.kt
+++ mottak/src/main/kotlin/no/nav/melosys/soknadmottak/common/Exceptions.kt
@@ -8,17 +8,13 @@ private val logger = KotlinLogging.logger { }
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 class IkkeFunnetException : Exception {
-    constructor(melding: String, throwable: Throwable) : super(melding, throwable) {
-        logger.error(throwable) { melding }
-    }
+    constructor(melding: String, throwable: Throwable) : super(melding, throwable)
 
     constructor(melding: String) : super(melding) {
-        logger.error { melding }
+        logger.warn { melding }
     }
 
-    constructor(throwable: Throwable) : super(throwable) {
-        logger.error(throwable) { }
-    }
+    constructor(throwable: Throwable) : super(throwable)
 }
 
 class IntegrasjonException : Exception {
@@ -37,13 +33,6 @@ class PubliserSoknadException : Exception {
     }
 
     constructor(throwable: Throwable) : super(throwable) {
-        logger.error(throwable) {  }
-    }
-}
-
-@ResponseStatus(value = HttpStatus.FORBIDDEN)
-class SikkerhetsbegrensningException : Exception {
-    constructor(melding: String) : super(melding) {
-        logger.error { melding }
+        logger.error(throwable) { }
     }
 }
