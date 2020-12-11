17605a36d8a3dd1e239fadbb56d6f1edbb85cac4
Eivind Nilsbakken <eivind.nilsbakken@nav.no>
2020-12-11 17:39:12 +0100

    Initialiser metrikker i bønne for å gjøre dem tilgjengelig ved oppstart

--- mottak/src/main/kotlin/no/nav/melosys/soknadmottak/common/Metrikker.kt
+++ mottak/src/main/kotlin/no/nav/melosys/soknadmottak/common/Metrikker.kt
@@ -1,21 +1,9 @@
 package no.nav.melosys.soknadmottak.common
 
 import io.micrometer.core.instrument.Counter
-import io.micrometer.core.instrument.Metrics
-
-private const val NAMESPACE = "melosys_soknad_mottak"
 
 object Metrikker {
-    internal val søknadMottatt: Counter = Counter.builder("soknad-mottatt")
-        .baseUnit(NAMESPACE)
-        .description("Antall søknader mottatt fra Altinn")
-        .register(Metrics.globalRegistry).apply { increment() }
-    internal val kvitteringSendt: Counter = Counter.builder("kvittering-sendt")
-        .baseUnit(NAMESPACE)
-        .description("Antall kvitteringer sendt til søker")
-        .register(Metrics.globalRegistry).apply { increment() }
-    internal val meldingSendt: Counter = Counter.builder("melding-sendt")
-        .baseUnit(NAMESPACE)
-        .description("Antall søknader sendt til Melosys")
-        .register(Metrics.globalRegistry).apply { increment() }
+    internal lateinit var søknadMottatt: Counter
+    internal lateinit var kvitteringSendt: Counter
+    internal lateinit var meldingSendt: Counter
 }
--- mottak/src/main/kotlin/no/nav/melosys/soknadmottak/config/MetrikkConfig.kt
+++ mottak/src/main/kotlin/no/nav/melosys/soknadmottak/config/MetrikkConfig.kt
@@ -1,9 +1,16 @@
 package no.nav.melosys.soknadmottak.config
 
+import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Metrics
+import no.nav.melosys.soknadmottak.common.Metrikker.kvitteringSendt
+import no.nav.melosys.soknadmottak.common.Metrikker.meldingSendt
+import no.nav.melosys.soknadmottak.common.Metrikker.søknadMottatt
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
+private const val NAMESPACE = "melosys_soknad_mottak"
 
 @Configuration
 class MetrikkConfig {
@@ -13,5 +20,21 @@ class MetrikkConfig {
         return MeterRegistryCustomizer { registry: MeterRegistry ->
             registry.config().commonTags("app", "melosys-soknad-mottak", "team", "teammelosys")
         }
+    }
+
+    @Bean
+    fun initialiserMetrikker() {
+        søknadMottatt = Counter.builder("soknad-mottatt")
+            .baseUnit(NAMESPACE)
+            .description("Antall søknader mottatt fra Altinn")
+            .register(Metrics.globalRegistry)
+        kvitteringSendt = Counter.builder("kvittering-sendt")
+            .baseUnit(NAMESPACE)
+            .description("Antall kvitteringer sendt til søker")
+            .register(Metrics.globalRegistry)
+        meldingSendt = Counter.builder("melding-sendt")
+            .baseUnit(NAMESPACE)
+            .description("Antall søknader sendt til Melosys")
+            .register(Metrics.globalRegistry)
     }
 }
