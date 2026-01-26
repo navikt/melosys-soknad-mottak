22a90b6cec0ac7da88b34a80e5fd1a36ec4bf6c6
ck-hui <Chak.Kwan.Hui@nav.no>
2026-01-26 11:54:47 +0100

    Lagt til response DTO

--- mottak/src/main/kotlin/no/nav/melosys/soknadmottak/mottak/MottakAdminController.kt
+++ mottak/src/main/kotlin/no/nav/melosys/soknadmottak/mottak/MottakAdminController.kt
@@ -2,6 +2,7 @@ package no.nav.melosys.soknadmottak.mottak
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.melosys.soknadmottak.config.AltinnConfig
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -12,23 +13,29 @@ import org.springframework.web.bind.annotation.RestController
 
 private val logger = KotlinLogging.logger { }
 
+@Unprotected
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/status")
 class MottakAdminController @Autowired constructor(
     private val mottakService: MottakService,
     private val altinnConfig: AltinnConfig
 ) {
-    @GetMapping("/downloadqueue")
+    @GetMapping
     fun sjekkDownloadQueueStatus(
         @RequestParam(required = false) serviceCode: String?
-    ): ResponseEntity<String> {
+    ): ResponseEntity<DownloadQueueStatusDto> {
         val code = serviceCode ?: altinnConfig.downloadQueue.code
         return try {
             mottakService.getDownloadQueueItems(code)
-            ResponseEntity.ok("OK")
+            ResponseEntity.ok(DownloadQueueStatusDto(status = "UP"))
         } catch (t: Throwable) {
             logger.warn(t) { "Klarte ikke å hente download queue items fra Altinn" }
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERROR")
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(DownloadQueueStatusDto(status = "DOWN"))
         }
     }
 }
+
+data class DownloadQueueStatusDto(
+    val status: String,
+)
